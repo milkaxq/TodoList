1aaca3d2016c7d23ebb7173dc81ccd72455f8c34
milkaxq <extradamage2@gmail.com>
2023-05-14 16:08:44 +0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Eziz Abdylow on 14.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
